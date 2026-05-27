bb798c20e3015964163647d9e56d57d21926d48c
Clinton Zablon <clintonzablon99@gmail.com>
2026-05-27 13:07:35 +0300

    Add files via upload

---
+++ Google Search box/index.js
@@ -0,0 +1,6 @@
+let inputEl = document.getElementById("input")
+let searchBtnEl = document.getElementById("searchBtn")
+
+searchBtnEl.addEventListener("click", ()=>{
+    inputEl.classList.toggle("hide")
+})
